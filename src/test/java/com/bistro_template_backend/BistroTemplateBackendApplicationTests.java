f25382caa9ef214b0d8d9b4b0bb690b3651eb966
Paul Jreij <pjreij98@gmail.com>
2025-03-23 01:05:55 -0500

    Bistro Template first commit

---
+++ src/test/java/com/bistro_template_backend/BistroTemplateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bistro_template_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BistroTemplateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
